e8b0be5fde3f03b8c21ac73ee12ecf57e0b8a077
henricker <henriquevieira.siqxd@gmail.com>
2020-12-22 10:49:07 -0300

    List and delete Post!!

--- server.js
+++ server.js
@@ -25,14 +25,41 @@ server.get('/cad', (request, response) => {
     response.render('form');
 })
 
+server.get('/', (request, response) => {
+    
+    Post
+    .findAll({order: [['id', 'DESC']]})
+    .then((objs) => {
+        const posts = [];
+        objs.forEach((obj) => {
+            const dict = {
+                'id': obj.id,
+                'title': obj.title,
+                'content': obj.content
+            };
+            posts.push(dict);
+        })
+        response.render('home', {posts: posts});
+    });
+});
+
 server.post('/add', (request, response) => {
     Post.create({
         title: request.body.title,
         content: request.body.content,
     }).then(() => {
-        response.send("Post created with sucessfully!");
+        response.redirect('/');
     }).catch((err) => {
         response.send(`Error! ${err.message}`);
+    })
+})
+
+server.get('/remove/:id', (request, response) => {
+    Post.destroy({where: {id: request.params.id}})
+    .then(() => {
+        response.redirect('/');
+    }).catch((err) => {
+        response.send('Ocorreu um erro: ' + err);
     })
 })
 
