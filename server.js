0d5c55c56ed4eacd09564a003ef5a863191f3945
henricker <henriquevieira.siqxd@gmail.com>
2020-12-21 22:03:51 -0300

    Save post with sucessfully

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const server = express();
-const Sequelize = require('sequelize');
+
+//Import template engine 
 const handlebars = require('express-handlebars'); 
+
+//Import body-parser, to take data from form
 const bodyParser = require('body-parser');
 
+//Import models
+const Post = require('./models/Post');
 
 //config 
     
@@ -15,20 +20,20 @@ const bodyParser = require('body-parser');
     server.use(bodyParser.urlencoded({extended: false}));
     server.use(bodyParser.json());
 
-//Connection with mysql database
-    const sequelize = new Sequelize('test', 'root', '32317461Henrique@', {
-        host: 'localhost',
-        dialect: 'mysql'
-    })
-
 //routes
 server.get('/cad', (request, response) => {
     response.render('form');
 })
 
 server.post('/add', (request, response) => {
-    response.send(request.body);
-    //res.send("Data received!");
+    Post.create({
+        title: request.body.title,
+        content: request.body.content,
+    }).then(() => {
+        response.send("Post created with sucessfully!");
+    }).catch((err) => {
+        response.send(`Error! ${err.message}`);
+    })
 })
 
 //Running server in 8080 port localhost.
